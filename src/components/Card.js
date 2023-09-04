9138c566091298d40ebcd405ba727de976e1673d
AMAL RAJ T <amal.art.art@gmail.com>
2023-09-04 13:47:12 +0530

    ui edited

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,8 @@ import { IoBed } from "react-icons/io5";
 import { BiBath } from "react-icons/bi";
 
 function Card(props) {
-  const { imageUrl, price, location, address, beds, bathrooms, dimensions } = props;
+  const { imageUrl, price, location, address, beds, bathrooms, dimensions } =
+    props;
 
   return (
     <div className="relative w-[100%] md:w-[calc(33.33% - 8px)] lg:w-[calc(33.33% - 16px)] h-[100%] rounded-[20px] flex flex-col items-start justify-start mb-4">
@@ -27,7 +28,7 @@ function Card(props) {
           <IoBed />
           <h1 className="text-[15px] font-medium text-gray-500 py-1">{beds}</h1>
         </div>
-        <div className="w-[25%] h-full flex flex-row items-center justify-around">
+        <div className="w-[31%] h-full flex flex-row items-center justify-around">
           <BiBath />
           <h1 className="text-[15px] font-medium text-gray-500 py-1">
             {bathrooms} bathrooms
